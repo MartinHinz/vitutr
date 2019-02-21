9a5815e9cf37e369b5d34fa55297efb810daf6a0
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2019-02-21 13:03:38 +0100

    next try to convince github pages to accept categories

--- Gemfile
+++ Gemfile
@@ -21,7 +21,6 @@ gem "minima", "~> 2.0"
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.6"
   gem "jekyll-paginate"
-  gem "jekyll-category-pages"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
@@ -30,4 +29,4 @@ gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 # Performance-booster for watching directories on Windows
 gem "wdm", "~> 0.1.0" if Gem.win_platform?
 
-gem "github-pages", "~> 193", group: :jekyll_plugins
+# gem "github-pages", "~> 193", group: :jekyll_plugins
--- _plugins/categories_tags_generator.rb
+++ _plugins/categories_tags_generator.rb
@@ -2,7 +2,6 @@ module Jekyll
   class TagsGenerator < Generator
 
     def generate(site)
-      puts "Hallo"
         tags_dir = Dir.pwd + '/tags'
 
         if !Dir.exists?(tags_dir)
@@ -17,6 +16,37 @@ module Jekyll
                 tag_file = File.new(tags_dir + '/' + i[0] + '.html', "w")
                 tag_file.puts("---\nlayout: blog_by_tag\ntag: " + i[0] + "\npermalink: /tags/" + i[0] + "/\n---")
                 tag_file.close
+
+                regenerate_flag = true
+            end
+        end
+
+        if regenerate_flag
+            FileUtils.touch Dir.pwd+'/_config.yml'
+        end
+
+    end
+  end
+end
+
+module Jekyll
+  class CategoriesGenerator < Generator
+
+    def generate(site)
+        categories_dir = Dir.pwd + '/categories'
+
+        if !Dir.exists?(categories_dir)
+            puts "Creating categories dir"
+            Dir.mkdir(categories_dir)
+        end
+        regenerate_flag = false
+
+        site.categories.each do |i|
+            if !File.exists?(categories_dir + '/' + CGI.escape(i[0]) + '.html')
+                puts "Creating categories page for: " + i[0]
+                category_file = File.new(categories_dir + '/' + CGI.escape(i[0]) + '.html', "w")
+                category_file.puts("---\nlayout: blog_by_category\ncategory: " + i[0] + "\npermalink: /categories/" + CGI.escape(i[0]) + "/\n---")
+                category_file.close
 
                 regenerate_flag = true
             end
